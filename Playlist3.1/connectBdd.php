7e2bf83977f20a29b28ac3966e24708da9763945
shahora-n <lengaigne.n@gmail.com>
2016-11-09 08:27:04 +0100

    On va forker !

--- Playlist3.1/connectBdd.php
+++ Playlist3.1/connectBdd.php
@@ -2,7 +2,7 @@
 <?php
 define('SQL_DSN', 'mysql:dbname=testlist;host=localhost;chaset=utf8');
 define('SQL_USERNAME', 'root');
-define('SQL_PASSWORD', 'admin');
+define('SQL_PASSWORD', '1234');
 
 $pdo= new PDO(SQL_DSN, SQL_USERNAME, SQL_PASSWORD,
       array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
